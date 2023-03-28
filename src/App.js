932308c2cc4b5b7386c72e750a95c9f0a2ed1642
YareliB07 <yarelibravo84@gmail.com>
2023-03-28 10:08:16 -0600

    update from-react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yareli
+          Yareliiiiii - Bravo 
         </p>
         <a
           className="App-link"
